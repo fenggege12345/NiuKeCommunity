f33e471a92dd8c49de86d710a735181b0544678c
guofeng liu <937413907@qq.com>
2022-04-20 22:29:12 +0800

    用户查询

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -1,4 +1,4 @@
-package com.nowcoder.community.community.controller;
+package com.nowcoder.community.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/test/java/com/nowcoder/community/CommunityApplicationTests.java
+++ src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nowcoder.community.community;
+package com.nowcoder.community;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
