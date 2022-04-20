86c2dd7f73b6c2ea935b6b7c329d40597e61a6d0
guofeng liu <937413907@qq.com>
2022-04-20 21:50:31 +0800

    添加依赖

--- src/main/java/com/nowcoder/community/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/community/CommunityApplication.java
@@ -8,7 +8,6 @@ public class CommunityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
+		System.out.println("项目启动成功！！！！！！！！！！！！！！！！！！");
 	}
-
-
 }
