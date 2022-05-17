efc9ee68b120ae4c82b801c11c17c2929b448b79
wininghope <767296826@qq.com>
2022-05-17 12:22:07 +0800

    开发登录和退出功能

--- src/test/java/com/nowcoder/community/MapperTests.java
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -4,8 +4,10 @@ package com.nowcoder.community;
 
 
 import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.LoginTicketMapper;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.LoginTicket;
 import com.nowcoder.community.entity.User;
 
 import org.junit.Test;
@@ -15,6 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -29,6 +32,9 @@ public class MapperTests {
     @Autowired
     DiscussPostMapper discussPostMapper;
 
+    @Autowired
+    LoginTicketMapper loginTicketMapper;
+
     @Test
     public void testSelectUser() {
         User user = userMapper.selectById(101);
@@ -41,6 +47,28 @@ public class MapperTests {
         for (DiscussPost discussPost : discussPosts) {
             System.out.println(discussPost.toString());
         }
-
     }
+
+    @Test
+    public void testInsertLoginTicket(){
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(101);
+        loginTicket.setTicket("abc");
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000 * 60 * 10));
+
+        loginTicketMapper.insertLoginTicket(loginTicket);
+    }
+
+    @Test
+    public void testSelectLoginTicket(){
+        LoginTicket loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+
+        loginTicketMapper.updateStatus("abc",1);
+        loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+    }
+
+
 }
