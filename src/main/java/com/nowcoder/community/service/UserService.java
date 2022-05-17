8c895b6926b56ba5d324c510472aa88dcc7cdf93
wininghope <767296826@qq.com>
2022-05-17 22:36:44 +0800

    账号设置

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -165,4 +165,7 @@ public class UserService implements CommunityConstant {
         return loginTicketMapper.selectByTicket(ticket);
     }
 
+    public int updateHeader(int userId,String headUrl){
+        return userMapper.updateHeader(userId,headUrl);
+    }
 }
