29f6a31c0410fe4af1574da348227e0c176c86cd
wininghope <767296826@qq.com>
2022-05-18 23:26:16 +0800

    发布帖子

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -1,7 +1,9 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -13,4 +15,15 @@ public class AlphaController {
         return "hello SpringBoot";
     }
 
+    //ajax示例
+    @RequestMapping(path = "/ajax",method = RequestMethod.POST)
+    @ResponseBody
+    public String testAjax(String name,int age){
+        System.out.println(name);
+        System.out.println(age);
+        return CommunityUtil.getJSONString(0,"操作成功");
+    }
+
 }
+
+
--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -2,6 +2,7 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.HtmlUtils;
@@ -15,8 +16,8 @@ public class DiscussPostService {
     @Autowired
     private DiscussPostMapper discussPostMapper;
 
-//    @Autowired
-//    private SensitiveFilter sensitiveFilter;
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
 
     public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit) {
         return discussPostMapper.selectDiscussPosts(userId, offset, limit);
@@ -27,21 +28,21 @@ public class DiscussPostService {
         return discussPostMapper.selectDiscussPostRows(userId);
     }
 
-//    public int addDiscussPost(DiscussPost discussPost) {
-//        if (discussPost == null) {
-//            throw new IllegalArgumentException("参数不能为空");
-//        }
-//
-//        //转义HTML标记
-//        discussPost.setTitle(HtmlUtils.htmlEscape(discussPost.getTitle()));
-//        discussPost.setContent(HtmlUtils.htmlEscape(discussPost.getContent()));
-//        //过滤敏感词
-//        discussPost.setTitle(sensitiveFilter.filter(discussPost.getTitle()));
-//        discussPost.setContent(sensitiveFilter.filter(discussPost.getContent()));
-//
-//        return discussPostMapper.insertDiscussPost(discussPost);
-//
-//    }
+    public int addDiscussPost(DiscussPost discussPost) {
+        if (discussPost == null) {
+            throw new IllegalArgumentException("参数不能为空");
+        }
+
+        //转义HTML标记
+        discussPost.setTitle(HtmlUtils.htmlEscape(discussPost.getTitle()));
+        discussPost.setContent(HtmlUtils.htmlEscape(discussPost.getContent()));
+        //过滤敏感词
+        discussPost.setTitle(sensitiveFilter.filter(discussPost.getTitle()));
+        discussPost.setContent(sensitiveFilter.filter(discussPost.getContent()));
+
+        return discussPostMapper.insertDiscussPost(discussPost);
+
+    }
 
     public DiscussPost findDiscussPostById(int id) {
         return discussPostMapper.selectDiscussPostById(id);
